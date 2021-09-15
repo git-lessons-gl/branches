0520ec81ac292f338e6a24e5f5aee4a7e66333a2
Maksym Prymierov <maksym.v.prymierov@globallogic.com>
2021-09-15 15:11:49 +0300

    Check for negative numbers(cherry-pick from testing branch)
    Signed-off-by: Maksym Prymierov <maksym.v.prymierov@globallogic.com>

--- main.cpp
+++ main.cpp
@@ -11,6 +11,11 @@ int main()
         std::cout << "b: ";
         std::cin >> b;
 
+        if (a < 0 || b < 0) {
+                std::cout << "Invalid numbers" << std::endl;
+                return -1;
+        }
+
         std::cout << a << " + " << b << " = " << a + b << "\n";
 
         return 0;
