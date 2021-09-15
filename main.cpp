4c05c653384668c7f9692d3a21d6572680ff36f2
Maksym Prymierov <maksym.v.prymierov@globallogic.com>
2021-09-15 15:11:49 +0300

    Check for negative numbers
    Signed-off-by: Maksym Prymierov <maksym.v.prymierov@globallogic.com>

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@ int main()
         int a = 5;
         int b = 5;
 
+        if (a < 0 || b < 0) {
+                std::cout << "Invalid numbers" << std::endl;
+                return -1;
+        }
+
         std::cout << a << " + " << b << " = " << a + b << std::endl;
 
         return 0;
