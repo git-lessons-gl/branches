b36777290e960ddef0bf782dc9313d5402aa168f
Anna Salatska <annasalatska@gmail.com>
2021-09-16 17:16:38 +0300

    Resolved conflict
    Signed-off-by: Anna Salatska <annasalatska@gmail.com>

--- main.cpp
+++ main.cpp
@@ -5,11 +5,20 @@ int main()
         int a;
         int b;
 
+<<<<<<< HEAD
         std::cout << "a: ";
         std::cin >> a;
 
         std::cout << "b: ";
         std::cin >> b;
+=======
+        if (a < 0 || b < 0) {
+                std::cout << "Invalid numbers" << std::endl;
+                return -1;
+        }
+
+        std::cout << a << " + " << b << " = " << a + b << std::endl;
+>>>>>>> 4c05c65... Check for negative numbers
 
         std::cout << a << " + " << b << " = " << a + b << std::endl;
         std::cout << "My first commit" << std::endl;
