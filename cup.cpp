53ac569878f313dc5ca8c5a2d18ef317424221a4
Dima Horbenko <dimoviy@gmail.com>
2021-09-16 10:04:12 +0300

    Add Cup class
    Cup has basic properties: material, volume and size.
    Methods and constructors are not implemented yet.
    
    Signed-off-by: Dima Horbenko <dimoviy@gmail.com>

---
+++ cup.cpp
@@ -0,0 +1,9 @@
+#include "cup.h"
+
+class Cup
+{
+private:
+    cup_materal_t material;
+    int volume;
+    cup_size_t size;
+};
---
+++ cup.h
@@ -0,0 +1,28 @@
+#ifndef BRANCHES_CUP_H
+#define BRANCHES_CUP_H
+
+typedef enum
+{
+    GLASS,
+    METAL,
+    CERAMIC,
+    PLASTIC,
+
+    CUP_MATERIAL_ENUM_SIZE
+
+} cup_materal_t;
+
+typedef enum
+{
+    XS,
+    S,
+    M,
+    L,
+    XL,
+    XXL,
+
+} cup_size_t;
+
+
+
+#endif // BRANCHES_CUP_H
