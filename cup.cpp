a1cd29c8998333716fef3049ffce243ceadbb098
Dima Horbenko <dimoviy@gmail.com>
2021-09-16 10:17:44 +0300

    Add Cup constructor
    Signed-off-by: Dima Horbenko <dimoviy@gmail.com>

--- cup.cpp
+++ cup.cpp
@@ -6,4 +6,12 @@ private:
     cup_materal_t material;
     int volume;
     cup_size_t size;
+
+public:
+    Cup(cup_materal_t mat, int vol, cup_size_t sz)
+    {
+        material = mat;
+        volume = vol;
+        size = sz;
+    }
 };
