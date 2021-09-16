e0bee6082fe3d69e8ceb72e9402e75d2f1d7a760
Helli Jelli <olga19932507@gmail.come>
2021-09-16 17:20:55 +0300

    Refactoring. Added function Print().

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 int Sum(int& a,int& b){
     int res;
     return res = a+b;}
+void Print(int&a, int& b){
+    std::cout << a << " + " << b << " = " << Sum(a,b) << std::endl;
+    }
 int main()
 {
         int a;
@@ -13,7 +16,7 @@ int main()
         std::cout << "b: ";
         std::cin >> b;
 
-        std::cout << a << " + " << b << " = " << Sum(a,b) << std::endl;
+        Print(a,b);
 
         return 0;
 }
