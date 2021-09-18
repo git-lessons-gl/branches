d2c13e5e8a1a2e2652708abf3d38aacb21dde9f7
Andrii Baryshnychenko <andreyversa@gmail.com>
2021-09-18 10:58:39 +0300

    add function sumAndPrint and the ability to pass values from command line arguments
    Signed-off-by: Andrii Baryshnychenko <andreyversa@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <functional>
+#include <stdlib.h>
 
-int main()
+template<typename T>
+void sumAndPrint(T x, T y) {
+	std::cout << x << " + " << y << " = " << std::plus<T>()(x, y) << std::endl;
+}
+
+int main(int argc, char **argv)
 {
 	int a;
 	int b;
-
+	
+	if (argc == 3) {
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		sumAndPrint(a, b);
+		return 0;
+	}
+	
 	std::cout << "a: ";
 	std::cin >> a;
 
 	std::cout << "b: ";
 	std::cin >> b;
-
-	std::cout << a << " + " << b << " = " << std::plus<int>()(a, b) << std::endl;
-
+	
+	sumAndPrint(a, b);
+	
 	return 0;
 }
