232c8339f9209d50085c2e46902b343b5600b8cc
Dmytro Kozliuk <kzlk.dm@gmail.com>
2022-07-13 15:56:56 +0300

    Add compare number
    Signed-off-by: Dmytro Kozliuk <kzlk.dm@gmail.com>

--- main.cpp
+++ main.cpp
@@ -11,7 +11,11 @@ int main()
         std::cout << "b: ";
         std::cin >> b;
 
-        std::cout << a << " + " << b << " = " << a + b << std::endl;
-
+        if(a > b)
+                std::cout << "a greater then b : " << a << " + " << b << " = " << a + b << std::endl;
+        else if(b > a)
+                std::cout << "b greater then a : " << a << " + " << b << " = " << a + b << std::endl;
+        else
+                std::cout << "b is equal a : " << a << " + " << b << " = " << a + b << std::endl;
         return 0;
 }
