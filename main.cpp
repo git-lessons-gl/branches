a25de5badbf9ac0bbb98192388ae785521d93e67
Vitalii Romaniv <vitalii.romaniv03@gmail.com>
2022-07-15 18:06:15 +0300

    Second commit add sub and mul function

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@ int add_func(int &a, int &b)
         return a + b;
 }
 
+int sub_func(int &a, int &b)
+{
+        return a - b;
+}
+
+int mul_func(int &a, int &b)
+{
+        return a * b;
+}
+
 int main()
 {
         int a;
