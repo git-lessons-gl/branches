76ac0527444314bc9b6207edb16911a3843d4c77
Vitalii Romaniv <vitalii.romaniv03@gmail.com>
2022-07-15 18:13:27 +0300

    Last commit add do_and_print function

--- main.cpp
+++ main.cpp
@@ -15,10 +15,11 @@ int mul_func(int &a, int &b)
         return a * b;
 }
 
-int main()
+void do_and_print_func()
 {
-        int a;
-        int b;
+        int a{};
+        int b{};
+        int sign{};
 
         std::cout << "a: ";
         std::cin >> a;
@@ -26,7 +27,29 @@ int main()
         std::cout << "b: ";
         std::cin >> b;
 
-        std::cout << a << " + " << b << " = " << add_func(a,b) << std::endl;
+        std::cout << "Choose an operation:\n1. +\n2. -\n3. *\nYour choice: ";
+        std::cin >>  sign;
 
+        switch(sign)
+        {
+                case 1:
+                        std::cout << a << " + " << b << " = " << add_func(a,b) << std::endl;
+                        break;
+                case 2:
+                        std::cout << a << " - " << b << " = " << sub_func(a,b) << std::endl;
+                        break;
+                case 3:
+                        std::cout << a << " * " << b << " = " << mul_func(a,b) << std::endl;
+                        break;
+                default:
+                        std::cout << "Error! Try again" << std::endl;
+                        break;
+        }
+}
+
+
+int main()
+{
+        do_and_print_func();
         return 0;
 }
