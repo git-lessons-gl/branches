37f280f45c6869e280e98efbba365600108d528f
batiukmaks <batiukmaks@gmail.com>
2022-07-18 18:25:32 +0300

    Add multiplying

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main()
         std::cin >> b;
 
         std::cout << a << " + " << b << " = " << a + b << std::endl;
+        std::cout << a << " * " << b << " = " << a * b << std::endl;
 
         return 0;
 }
